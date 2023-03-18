b7d88a6fd07bb9fdce9b9c98fa5a64d2e30b1bdb
b06b01073 <b06b01073@gmail.com>
2023-03-18 23:16:42 +0800

    can now forward pass and calculate loss

--- lab1_backprop/backward.py
+++ lab1_backprop/backward.py
@@ -54,26 +54,17 @@ def show_result(x, y, pred_y):
 
 def main():
     x, y = generate_linear()
-    linear1 = module.Linear(in_features=3, out_features=20)
-    linear2 = module.Linear(in_features=20, out_features=1)
-    sigmoid = module.Sigmoid()
+    net = module.Net_HW1(in_features=2, out_features=1)
+    loss_func = module.LossFunction()
 
-    net = [linear1, sigmoid, linear2, sigmoid]
+    # epochs = 100
+    # for i in range(epochs):
+    pred = net.forward(x.T)
+        
+    loss = loss_func.forward(pred, y)
+    print(loss_func.backward(pred, y).shape)
 
-    epochs = 100
-    for i in range(epochs):
-        for data, label in zip(x, y):
-            pred = data
-            for layer in net:
-                print(layer)
-                pred = layer(pred)
-            loss = module.square_error(label, pred[0])
-            grad_w, grad_b = module.loss_grad(label, data, linear1)
-            linear1.w = linear1.w - 1e-4*grad_w
-            linear1.b = linear1.b - 1e-4*grad_b
-            print(loss)
-
-    show_result(x, y, pred)
+    # show_result(x, y, pred)
 
 if __name__ == '__main__':
     main()
--- lab1_backprop/module.py
+++ lab1_backprop/module.py
@@ -1,34 +1,55 @@
 import numpy as np
 
+class Net_HW1:
+    def __init__(self, in_features, out_features):
+        self.hidden_features = 128
+        self.linear1 = Linear(in_features, self.hidden_features)
+        self.linear2 = Linear(self.hidden_features, self.hidden_features)
+        self.linear3 = Linear(self.hidden_features, out_features)
+        self.sigmoid = Sigmoid()
+
+        self.net = [self.linear1, self.linear2, self.linear3]
+
+    def forward(self, x):
+        print(x.shape)
+        x = self.sigmoid.forward(self.net[0].forward(x))
+        x = self.sigmoid.forward(self.net[1].forward(x))
+        x = self.sigmoid.forward(self.net[2].forward(x))
+
+        return x
+
+    # def update(self, loss_func, pred, y):
+    #     for i in range(len(self.net) - 1, -1, -1)
+    #         layer = self.net[i]
+
 class Linear:
     def __init__(self, in_features, out_features):  
-        self.w = np.random.rand(in_features, out_features)
-        self.b = np.random.rand(out_features)
+
+        # the 1e-2 is required, otherwise the sigmoid will always output 1 and break the log function in loss function
+        self.w = np.random.rand(out_features, in_features) * 1e-2
+        # a dummy dimension is added for broadcasting
+        self.b = np.random.rand(out_features, 1)
 
     def forward(self, x):
-        return self.w.T @ x + self.b
+        return np.dot(self.w, x) + self.b
 
-    def __call__(self, x):
-        return self.forward(x)
-
+    # def backward(self, prev_derivative):
+    #     return prev_derivative * 
 
 class Sigmoid:
-
     def forward(self, x):
         return 1 / (1 + np.exp(-x))
-    def __call__(self, x):
-        return self.forward(x)
 
-def square_error(input1, input2):
-    return (input1 - input2) ** 2
+    # def backward(self, x):
 
-def loss_grad(y, x, linear_layer):
-    linear_output = linear_layer(x)
-    
-    sigmoid = Sigmoid()
-    sig_output = sigmoid(linear_output)
 
-    grad_w = -(y - sig_output) * sig_output * (1 - sig_output) * np.sum(x)
-    grad_b = -(y - sig_output) * sig_output * (1 - sig_output)
+class LossFunction:
+    def forward(self, pred, y):
 
-    return grad_w, grad_b
+        # y.shape is (100, 1)
+        dataset_size = y.shape[0]
+        loss = -(1 / dataset_size) * (np.dot(np.log(pred), y) + np.dot(np.log(1 - pred), (1 - y)))
+        return loss.squeeze()
+
+    def backward(self, pred, y):
+        return np.divide(1 - y, 1 - pred) - np.divide(y, pred)
