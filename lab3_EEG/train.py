29dd91d69d3e23e98e2669e3b07f443f4173e985
b06b01073 <b06b01073@gmail.com>
2023-04-08 06:23:36 +0800

    near 80

--- lab3_EEG/train.py
+++ lab3_EEG/train.py
@@ -6,6 +6,7 @@ import torch.optim as optim
 import torch.nn as nn
 import numpy as np
 from copy import deepcopy
+import matplotlib.pyplot as plt
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 print(f'Using {device}')
@@ -14,10 +15,17 @@ def augmentation(data_batches):
     augmented_batches = deepcopy(data_batches)
     for i in range(len(augmented_batches)):
         # print(augmented_batches[i].shape)
-        if np.random.uniform() > 0.6:
-            augmented_batches[i] += np.clip(np.random.normal(scale=5e-3, size=augmented_batches[i].shape), -1, 1)
-        # if np.random.uniform() > 0.8:
-        #     augmented_batches[i] = np.flip(augmented_batches[i], axis=2)
+        if np.random.uniform() > 0.5:
+            mean = np.mean(augmented_batches[i])
+            s = 5e-3
+            clipper = 1
+            augmented_batches[i] += np.clip(np.random.normal(loc=mean, scale=s, size=augmented_batches[i].shape), -clipper + mean, clipper + mean)
+        # if np.random.uniform() > 0:
+        #     shift = np.random.randint(low=0, high=3)
+        #     augmented_batches[i] = np.roll(augmented_batches[i], shift=shift, axis=3)
+        # if np.random.uniform() > 0.5:
+
+
 
     return augmented_batches
 
@@ -42,17 +50,23 @@ def get_batches(data, label, batch_size):
 
     return data_batches, label_batches
 
+def plot(train_acc, test_acc):
+    plt.plot(train_acc)
+    plt.plot(test_acc)
+    plt.savefig('acc.png')
+
 def train(data_batches, label_batches, model, optimizer, loss_fn, dataset_size):
     model.train()
     total_loss = 0
     corrects = 0
 
-    data_batches = augmentation(data_batches)
+    # data_batches = augmentation(data_batches)
 
     for mini_batch, labels in zip(data_batches, label_batches):
         mini_batch = torch.from_numpy(mini_batch).float().to(device)
         labels = torch.from_numpy(labels).type(torch.LongTensor).to(device)
         output = model(mini_batch)
+
 
         optimizer.zero_grad()
         loss = loss_fn(output, labels)
@@ -89,31 +103,42 @@ def main(args):
     train_data_batches, train_label_batches = get_batches(train_data, train_label, args.batch_size)
     test_data_batches, test_label_batches = get_batches(test_data, test_label, args.batch_size)
 
+
     epoch = args.epoch
 
     print(f'Train data shape: {train_data.shape}, Train label shape: {train_label.shape}')
     print(f'Test data shape: {test_data.shape}, Test label shape: {test_label.shape}')
 
     model = EEGNet(args.activation).to(device)
-    optimizer = optim.Adam(model.parameters(), lr=args.lr, weight_decay=1e-3)
+    optimizer = optim.Adam(model.parameters(), lr=args.lr, weight_decay=2e-3)
+    # optimizer = optim.RMSprop(model.parameters(), lr=args.lr, weight_decay=1e-3)
     loss_fn = nn.CrossEntropyLoss()
     scheduler = optim.lr_scheduler.MultiStepLR(optimizer, milestones=[100, 200], gamma=0.1)
 
     train_size = train_data.shape[0]
     test_size = test_data.shape[0]
 
+    train_accs = []
+    test_accs = []
+
+
     for i in range(epoch):
         train_loss, train_acc = train(train_data_batches, train_label_batches, model, optimizer, loss_fn, train_size)
         test_loss, test_acc = test(test_data_batches, test_label_batches, model, loss_fn, test_size)
         scheduler.step()
 
+        train_accs.append(train_acc)
+        test_accs.append(test_acc)
+
         print(f'Epoch: {i}')
         print(f'train loss: {train_loss}, train_acc: {train_acc}')
         print(f'test loss: {test_loss}, test_acc: {test_acc}')
 
+    plot(train_accs, test_accs)
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--lr', type=float, default=0.01)
+    parser.add_argument('--lr', type=float, default=1e-2)
     parser.add_argument('--epoch', '-e', type=int, default=300)
     parser.add_argument('--batch_size', '-b', type=int, default=64)
     parser.add_argument('--activation', '-a', type=str, default='relu')
